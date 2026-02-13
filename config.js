ba37ac7d1d887aa53a52c38d3d1ced44d36f7c85
Brian Scott <brian@solardev.ca>
2026-02-13 14:41:31 -0700

    Add Stats API URL

---
+++ config.js
@@ -0,0 +1,7 @@
+/**
+ * Site-wide config for Bragging Rights.
+ * Edit this file to set the Stats API URL so all visitors get live scores.
+ * After deploying the stats server to Render, set your URL here and redeploy.
+ */
+window.BRAG_CONFIG = window.BRAG_CONFIG || {};
+window.BRAG_CONFIG.statsApiUrl = 'https://bragging-rights.onrender.com';
